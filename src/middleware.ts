832402194da4ed46d37dbdfced7cde87098bf0fa
Waxer59 <78129249+Waxer59@users.noreply.github.com>
2025-02-11 11:00:52 +0100

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -24,3 +24,8 @@ export async function middleware(request: NextRequest) {
     }
   }
 }
+
+// Routes Middleware should not run on
+export const config = {
+  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)']
+};
